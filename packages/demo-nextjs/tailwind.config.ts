a5b1ac4b0c879d8654c547e75bae8c618188ec6f
Sagar Poudel <poudels14@gmail.com>
2024-05-28 04:41:49 -0700

    make weather nicer

--- packages/demo-nextjs/tailwind.config.ts
+++ packages/demo-nextjs/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@portal/reagent-react/**/*.{js,ts,jsx,tsx}",
--- packages/react/demo-agents/tools/Weather.tsx
+++ packages/react/demo-agents/tools/Weather.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 import { createAgentNode, z } from "@portal/reagent/agent";
+import { generateMock } from "@anatine/zod-mock";
+
+const outputSchema = z.object({
+  temperature: z.number().max(100),
+  unit: z.enum(["celsius", "fahrenheit"]),
+  conditions: z.enum([
+    "Clear/Sunny",
+    "Partly Cloudy",
+    "Cloudy",
+    "Overcast",
+    "Rain",
+  ]),
+  humidity: z.number().min(0).max(100),
+  windSpeed: z.number().max(150),
+  feelsLike: z.number().max(100),
+});
 
 const GetWeather = createAgentNode({
   id: "@portal/demo-agents/getWeather",
@@ -8,34 +24,66 @@ const GetWeather = createAgentNode({
   version: "0.0.1",
   input: z.object({
     date: z.string().datetime().optional().describe("Date time in ISO string"),
-    city: z.string(),
-    country: z.string(),
+    city: z.string().describe("Name of the city"),
+    country: z.string().describe("Name of the country"),
     unit: z.enum(["celsius", "fahrenheit"]),
   }),
-  output: z.object({
-    temperature: z.number(),
-    unit: z.enum(["celsius", "fahrenheit"]),
-  }),
+  output: outputSchema,
   async *execute(context, input) {
+    const randomData = generateMock(outputSchema);
     const weather = {
-      temperature: Math.floor(Math.random() * 50),
+      ...randomData,
+      date: input.date,
+      city: input.city,
+      country: input.country,
       unit: input.unit,
     };
-    context.render(
-      (props) => {
-        return (
-          <div>
-            <div>Temperature: {props.data.weather.temperature}</div>
-          </div>
-        );
-      },
-      {
-        weather,
-      }
-    );
+    context.render((props) => {
+      return <Component {...props.data} />;
+    }, weather);
 
     yield weather;
   },
 });
+
+const Component = (props: any) => {
+  const currentHour = new Date(props.datetime).getHours();
+  const isDayTime = currentHour >= 6 && currentHour < 18;
+  return (
+    <div className={`${isDayTime ? "" : "dark"}`}>
+      <div
+        className={`p-6 rounded-lg shadow-lg text-white bg-gradient-to-br from-yellow-100 dark:from-gray-800 via-50% via-blue-400 dark:via-gray-800 to-blue-600 dark:to-gray-800 transition-colors duration-500`}
+      >
+        <div className="flex justify-between items-center mb-4">
+          <div>
+            <h2 className="text-3xl font-bold mb-1 text-slate-600 dark:text-slate-300">
+              {props.city}, {props.country}
+            </h2>
+            <p className="text-xl">{props.condition}</p>
+          </div>
+          <div className="text-right">
+            <p className="text-6xl font-semibold dark:text-slate-200">
+              {props.temperature}°C
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-around text-lg text-slate-100 dark:text-slate-300">
+          <div className="flex flex-col items-center">
+            <span className="font-semibold">Humidity</span>
+            <span>{props.humidity}%</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span>Wind</span>
+            <span>{props.windSpeed} km/h</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span>Feels Like</span>
+            <span>{props.feelsLike}°C</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export { GetWeather };
