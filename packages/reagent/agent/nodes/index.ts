c6eee67a9ff89757fdccf67a8f3243b68cccb03e
Sagar Poudel <poudels14@gmail.com>
2024-05-28 02:10:29 -0700

    oops add missing export

--- packages/reagent/agent/nodes/index.ts
+++ packages/reagent/agent/nodes/index.ts
@@ -1,4 +1,5 @@
 export { default as Passthrough } from "./Passthrough";
 export { default as ChatInput } from "./ChatInput";
 export { default as ChatCompletion } from "./ChatCompletion";
+export { default as ChatCompletionWithTools } from "./ChatCompletionWithTools";
 export { default as User } from "./User";
