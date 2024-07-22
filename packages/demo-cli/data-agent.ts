ceb4c7db89767a1a0522130d82a01982a7e8d2d5
Sagar Poudel <poudels14@gmail.com>
2024-07-22 13:27:53 -0700

    fix demos

--- packages/demo-cli/data-agent.ts
+++ packages/demo-cli/data-agent.ts
@@ -1,9 +1,4 @@
-// import agent, { nodes } from "@reagentai/react-examples/e2b.js";
-import agent, { nodes } from "@reagentai/react-examples/weather/index.js";
-// const nodes: any[] = [];
-// import agent, { nodes } from "@reagentai/react-examples/weather/index.js";
+import agent, { nodes } from "@reagentai/react-examples/e2b.js";
 export default agent;
 export { nodes };
 export const __reagentai_exports__ = true;
-
-// const x = () => <div clas/group-has-[.role-user]:bg-red-400
--- packages/demo-nextjs/app/api/chat/route.ts
+++ packages/demo-nextjs/app/api/chat/route.ts
@@ -1,6 +1,6 @@
-import { z } from "@reagentai/reagent/agent";
-import { OpenAI } from "@reagentai/reagent/llm/integrations/models";
-import { invokeGraphAgent } from "@reagentai/serve";
+import { z } from "@reagentai/reagent";
+import { OpenAI } from "@reagentai/reagent/llm/models";
+import { runReagentWorkflow } from "@reagentai/serve";
 import agent from "@reagentai/react-examples/e2b";
 
 const invokeSchema = z.object({
@@ -24,12 +24,12 @@ export async function POST(request: Request) {
     model: "gpt-3.5-turbo",
   });
 
-  const agentOutputStream = invokeGraphAgent<any>(agent, {
+  const workflowOutput = runReagentWorkflow<any>(agent, {
     nodeId: "input",
     input: {
       query: input.message.content,
       model,
     },
   });
-  return agentOutputStream.toResponse();
+  return workflowOutput.toResponse();
 }
