1c0d82853a44cb13693bef0a6699db0e2068f6c0
Sagar Poudel <poudels14@gmail.com>
2024-10-09 18:47:26 -0700

    react: memo bunch of components

--- packages/react/chat/ChatThread.tsx
+++ packages/react/chat/ChatThread.tsx
@@ -1,4 +1,12 @@
-import { useContext, useEffect, useMemo, useRef, useState } from "react";
+import {
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useStore } from "zustand";
 import clsx from "clsx";
 import Markdown from "react-markdown";
@@ -8,236 +16,284 @@ import { AgentNodeRenderer } from "./node.js";
 import { ChatStore } from "./state.js";
 import { useChatTheme } from "./theme.js";
 
-const ChatThread = (props: {
-  store: ChatStore;
-  EmptyScreen?: React.ReactNode;
-  Loader?: React.ReactNode;
-}) => {
-  const theme = useChatTheme();
-  const { classNames } = theme;
-  const { messages, sortedMessageIds, prompt, inflightRequest, error } =
-    useStore(props.store);
-  const sortedMessages = useMemo(() => {
-    return sortedMessageIds.map((id) => messages[id]);
-  }, [messages, sortedMessageIds]);
+const ChatThread = memo(
+  (props: {
+    store: ChatStore;
+    EmptyScreen?: React.ReactNode;
+    Loader?: React.ReactNode;
+  }) => {
+    let chatMessagesContainerRef = useRef<HTMLDivElement>(null);
+    let chatMessagesRef = useRef<HTMLDivElement>(null);
+    const theme = useChatTheme();
+    const { classNames } = theme;
 
-  let chatMessagesContainerRef = useRef<HTMLDivElement>(null);
-  let chatMessagesRef = useRef<HTMLDivElement>(null);
+    const scrollToBottom = useCallback(() => {
+      if (chatMessagesRef.current && chatMessagesContainerRef.current) {
+        const containerHeight = parseFloat(
+          getComputedStyle(chatMessagesRef.current!).height
+        );
+        chatMessagesContainerRef.current!.scrollTo({
+          top: containerHeight + 100_000,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    }, []);
 
-  const [lastMessage, setLastMessage] = useState(null);
-  const scrollToBottom = () => {
-    if (chatMessagesRef.current && chatMessagesContainerRef.current) {
-      const containerHeight = parseFloat(
-        getComputedStyle(chatMessagesRef.current!).height
-      );
-      chatMessagesContainerRef.current!.scrollTo({
-        top: containerHeight + 100_000,
-        left: 0,
-        behavior: "smooth",
-      });
-    }
-  };
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [
-    sortedMessages,
-    chatMessagesRef.current,
-    chatMessagesContainerRef.current,
-  ]);
-  return (
-    <div
-      ref={chatMessagesContainerRef}
-      className={clsx("chat-thread overflow-y-auto", classNames.thread)}
-    >
-      <div className="flex justify-center items-center">
-        <div
-          className={clsx(
-            "chat-messages-container flex-1",
-            classNames.messagesContainer
-          )}
-        >
-          {sortedMessageIds.length == 0 && props.EmptyScreen}
-          {sortedMessages.length > 0 && (
+    useEffect(() => {
+      scrollToBottom();
+    }, [chatMessagesRef.current, chatMessagesContainerRef.current]);
+    return (
+      <div
+        ref={chatMessagesContainerRef}
+        className={clsx("chat-thread overflow-y-auto", classNames.thread)}
+      >
+        <div className="flex justify-center items-center">
+          <div
+            className={clsx(
+              "chat-messages-container flex-1",
+              classNames.messagesContainer
+            )}
+          >
+            <EmptyScreen store={props.store} EmptyScreen={props.EmptyScreen} />
             <div
               ref={chatMessagesRef}
               className={clsx("chat-messages", classNames.messages)}
             >
-              {sortedMessages.map((message, index) => {
-                const isLastMessage = index == sortedMessages.length - 1;
-                if (isLastMessage && lastMessage !== message) {
-                  setLastMessage(message as any);
-                  scrollToBottom();
-                }
-                return (
-                  <ChatMessage
-                    key={index}
-                    message={message}
-                    store={props.store}
-                    showRole={
-                      index == 0 ||
-                      sortedMessages[index - 1].role != message.role
-                    }
-                    theme={theme}
-                  />
-                );
-              })}
-              {props.Loader &&
-                inflightRequest &&
-                !inflightRequest.responseReceived && (
-                  <ChatMessage
-                    message={{
-                      id: "loading",
-                      role: "ai",
-                      Loader: props.Loader,
-                    }}
-                    store={props.store}
-                    showRole={true}
-                    theme={theme}
-                  />
-                )}
-
-              {prompt?.Component && (
-                <ChatMessage
-                  message={{
-                    id: "prompt",
-                    role: "ai",
-                    prompt,
-                  }}
-                  store={props.store}
-                  showRole={false}
-                  theme={theme}
-                />
-              )}
-              {error && (
-                <div className="px-4 py-3 text-center text-base text-red-700 overflow-hidden text-ellipsis">
-                  Error sending message: {error}
-                </div>
-              )}
+              <ChatMessages
+                store={props.store}
+                scrollToBottom={scrollToBottom}
+                Loader={props.Loader}
+              />
+              <PromptComponent store={props.store} />
+              <Error store={props.store} />
             </div>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+    );
+  }
+);
+
+const EmptyScreen = (props: {
+  store: ChatStore;
+  EmptyScreen?: React.ReactNode;
+}) => {
+  const { sortedMessageIds } = useStore(props.store);
+  return <>{sortedMessageIds.length == 0 && props.EmptyScreen}</>;
+};
+
+const Error = (props: { store: ChatStore }) => {
+  const { error } = useStore(props.store);
+  return (
+    <>
+      {error && (
+        <div className="px-4 py-3 text-center text-base text-red-700 overflow-hidden text-ellipsis">
+          Error sending message: {error}
+        </div>
+      )}
+    </>
   );
 };
 
-const ChatMessage = (props: {
-  message:
-    | (Pick<Chat.Message, "id" | "message" | "ui" | "role" | "node"> & {
-        Loader?: React.ReactNode;
-        prompt?: undefined;
-      })
-    | {
-        id: string;
-        role: "ai";
-        Loader?: React.ReactNode;
-        prompt?: any;
-        ui?: undefined;
-        message?: undefined;
-        node?: undefined;
-      };
+const ChatMessages = (props: {
   store: ChatStore;
-  showRole: boolean;
-  theme: ReturnType<typeof useChatTheme>;
+  scrollToBottom: () => void;
+  Loader?: React.ReactNode;
 }) => {
-  const theme = props.theme;
-  const { classNames } = theme;
-  const markdownRef = useRef<HTMLDivElement>(null);
-  const role = useMemo(() => {
-    const id = props.message.role || "user";
-    return {
-      id,
-      name:
-        id == "ai"
-          ? theme.avatars.ai
-          : id == "system"
-            ? null
-            : theme.avatars.user,
-    };
-  }, [props.message.role]);
+  const theme = useChatTheme();
+  const { messages, sortedMessageIds, inflightRequest } = useStore(props.store);
+  const sortedMessages = useMemo(() => {
+    return sortedMessageIds.map((id) => messages[id]);
+  }, [messages, sortedMessageIds]);
+
+  const [lastMessage, setLastMessage] = useState(null);
+  useEffect(() => {
+    props.scrollToBottom();
+  }, [sortedMessages]);
 
   return (
-    <div
-      className={clsx(
-        "chat-message-container group flex flex-row",
-        classNames.messageContainer,
-        {
-          "!mt-0 pt-2": !props.showRole,
-        }
+    <>
+      {sortedMessages.length > 0 &&
+        sortedMessages.map((message, index) => {
+          const isLastMessage = index == sortedMessages.length - 1;
+          if (isLastMessage && lastMessage !== message) {
+            setLastMessage(message as any);
+            props.scrollToBottom();
+          }
+          return (
+            <ChatMessage
+              key={message.id}
+              message={message}
+              store={props.store}
+              showRole={
+                index == 0 || sortedMessages[index - 1].role != message.role
+              }
+              theme={theme}
+            />
+          );
+        })}
+      {props.Loader && inflightRequest && !inflightRequest.responseReceived && (
+        <ChatMessage
+          message={{
+            id: "loading",
+            role: "ai",
+            Loader: props.Loader,
+          }}
+          store={props.store}
+          showRole={true}
+          theme={theme}
+        />
       )}
-      data-role={role.id}
-    >
-      <div
-        className={clsx("role-container", role.id, classNames.roleContainer)}
-        data-role={role.id}
-      >
-        {props.showRole && role.name && (
-          <div
-            className={clsx("role select-none text-center", classNames.role)}
-          >
-            {role.name}
-          </div>
-        )}
-      </div>
+    </>
+  );
+};
+
+const PromptComponent = memo((props: { store: ChatStore }) => {
+  const prompt = useStore(props.store, (s) => s.prompt);
+
+  const theme = useChatTheme();
+  const message = useMemo(() => {
+    return {
+      id: "prompt",
+      role: "ai",
+      prompt,
+    };
+  }, [prompt]);
+  if (!prompt?.Component) {
+    return null;
+  }
+  return (
+    <ChatMessage
+      message={message as any}
+      store={undefined!}
+      showRole={false}
+      theme={theme}
+    />
+  );
+});
+
+const ChatMessage = memo(
+  (props: {
+    message:
+      | (Pick<Chat.Message, "id" | "message" | "ui" | "role" | "node"> & {
+          Loader?: React.ReactNode;
+          prompt?: undefined;
+        })
+      | {
+          id: string;
+          role: "ai";
+          Loader?: React.ReactNode;
+          prompt?: any;
+          ui?: undefined;
+          message?: undefined;
+          node?: undefined;
+        };
+    store: ChatStore;
+    showRole: boolean;
+    theme: ReturnType<typeof useChatTheme>;
+  }) => {
+    const theme = props.theme;
+    const { classNames } = theme;
+    const markdownRef = useRef<HTMLDivElement>(null);
+    const role = useMemo(() => {
+      const id = props.message.role || "user";
+      return {
+        id,
+        name:
+          id == "ai"
+            ? theme.avatars.ai
+            : id == "system"
+              ? null
+              : theme.avatars.user,
+      };
+    }, [props.message.role]);
+
+    return (
       <div
         className={clsx(
-          "chat-message flex-1 overflow-x-hidden",
-          classNames.message
+          "chat-message-container group flex flex-row",
+          classNames.messageContainer,
+          {
+            "!mt-0 pt-2": !props.showRole,
+          }
         )}
-        data-message-id={props.message.id}
         data-role={role.id}
       >
-        {props.message.Loader && (
-          <div
-            className={clsx("chat-message-loading", classNames.messageContent)}
-            data-role={role.id}
-          >
-            {props.message.Loader}
-          </div>
-        )}
-        {props.message.ui &&
-          props.message.ui.map((ui, index) => (
-            <div className="chat-message-ui" key={index}>
-              <AgentNodeRenderer
-                messageId={props.message.id}
-                store={props.store}
-                node={props.message.node}
-                ui={ui}
-              />
-            </div>
-          ))}
-        {
-          // node isn't set for user message
-          (Boolean(props.message.message) ||
-            props.message.node?.type == "@core/chat-completion") && (
+        <div
+          className={clsx("role-container", role.id, classNames.roleContainer)}
+          data-role={role.id}
+        >
+          {props.showRole && role.name && (
             <div
-              ref={markdownRef}
+              className={clsx("role select-none text-center", classNames.role)}
+            >
+              {role.name}
+            </div>
+          )}
+        </div>
+        <div
+          className={clsx(
+            "chat-message flex-1 overflow-x-hidden",
+            classNames.message
+          )}
+          data-message-id={props.message.id}
+          data-role={role.id}
+        >
+          {props.message.Loader && (
+            <div
               className={clsx(
-                "chat-message-content prose select-text",
+                "chat-message-loading",
                 classNames.messageContent
               )}
               data-role={role.id}
             >
-              <Markdown remarkPlugins={[]}>
-                {props.message.message!.content}
-              </Markdown>
+              {props.message.Loader}
             </div>
-          )
-        }
-        {props.message.prompt && (
-          <props.message.prompt.Component
-            {...props.message.prompt.props}
-            React={{
-              useContext,
-              useEffect,
-              useMemo,
-            }}
-          />
-        )}
+          )}
+          {props.message.ui &&
+            props.message.ui.map((ui, index) => (
+              <div className="chat-message-ui" key={index}>
+                <AgentNodeRenderer
+                  messageId={props.message.id}
+                  store={props.store}
+                  node={props.message.node}
+                  ui={ui}
+                />
+              </div>
+            ))}
+          {
+            // node isn't set for user message
+            (Boolean(props.message.message) ||
+              props.message.node?.type == "@core/chat-completion") && (
+              <div
+                ref={markdownRef}
+                className={clsx(
+                  "chat-message-content prose select-text",
+                  classNames.messageContent
+                )}
+                data-role={role.id}
+              >
+                <Markdown remarkPlugins={[]}>
+                  {props.message.message!.content}
+                </Markdown>
+              </div>
+            )
+          }
+          {props.message.prompt && (
+            <props.message.prompt.Component
+              {...props.message.prompt.props}
+              React={{
+                useContext,
+                useEffect,
+                useMemo,
+              }}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export { ChatThread };
